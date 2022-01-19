2f8e0ad22f3c12b1fb56100a246c0c51797ea620
MILLION BERHAN <99065113@mydavinci.com>
2022-01-19 09:32:27 +0100

    date: 19-1-2022 Bag of M&Ms

--- Projecten/F1.6.01.O1 - Repository + labs maken/2.Rekenen met lists.py
+++ Projecten/F1.6.01.O1 - Repository + labs maken/2.Rekenen met lists.py
@@ -1,5 +1,6 @@
 from typing import Dict
 import random
+
 print ("2.rekenen met lists")
 
 listOne = [q for q in range(1,11,1)]
@@ -10,43 +11,35 @@ def geforceerde_input(vraag: str, toegestaan: list[str],
     while not ((antwoord := input(vraag).lower()) in toegestaan):
         print(niet_goed_bericht)
 
-def plussommen(): 
+def plussommen():  # hier worden de plussommen getoont
     for q in range(min(len(listOne), len(listTwo))):
         print("{:<2} + {:<2} = {:<5}".format(listOne[q], listTwo[q], listOne[q] + listTwo[q]))
 plussommen()
 
 print ("---------------------")
 
-def minsommen():
+def minsommen(): # hier worden de minsommen getoont
     for q in range(min(len(listOne), len(listTwo))):
         print("{:<2} - {:<2} = {:<5}".format(listOne[q], listTwo[q], listOne[q] - listTwo[q]))
 minsommen()
 
 print ("----------------------")
 
-def keersommen():
+def keersommen(): # hier worden de keersommen getoont
     for q in range(min(len(listOne), len(listTwo))):
         print("{:<2} * {:<2} = {:<5}".format(listOne[q], listTwo[q], listOne[q] * listTwo[q]))
 keersommen()
 
 print ("----------------------")
 
-def delendoor():
+def delendoor():# hier worden de delendoor sommen getoont
     for q in range(min(len(listOne), len(listTwo))):
         print("{:<2} / {:<2} = {:<5}".format(listOne[q], listTwo[q], listOne[q] / listTwo[q]))
 delendoor()
 
 print ("----------------------")
-
+# hier komt er een randomlist van 3 t/m 10 
 spelList= ['Monopoly', 'Yathzee', 'Bridge', 'Poker', 'Pesten', 'Mens erger je niet','Cluedo']
-
-# def spelProgramma():
-#     spelvraag = geforceerde_input("hoeveel spellen wilt u zien? :", [str(i) for i in range(3,10)])
-#     if spelvraag >2:
-#         print(random.randint(spelList))
-#     else:
-#         print (spelvraag)
-# spelProgramma()
 
 def spelProgramma(spelList, minimum, maximum):
     rng=random.choice(range(minimum, maximum))
